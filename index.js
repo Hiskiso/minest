e8ec49a8d3ae70c772b9fd82636a61b61ea1b39e
Hiskiso <roma123qq@gmail.com>
2023-07-26 17:52:15 +0400

    Everything is done

--- index.js
+++ index.js
@@ -36,7 +36,9 @@ function generateField(size, bombsCount) {
 let Mask = {
     Transparent: null,
     Fill: "",
-    Flag: "🚩"
+    Flag: "🚩",
+    lose: "lose",
+    sucess: "sucess"
 }
 
 
@@ -48,22 +50,92 @@ function gameStart() {
     let field = document.getElementsByClassName("field")[0]
     field.style.gridTemplateColumns = "repeat(" + fieldSize + ",1fr)"
 
-    mask = new Array(fieldSize * fieldSize).fill(Mask.Fill)
-
-    console.log(mask);
-
+    let mask = new Array(fieldSize * fieldSize).fill(Mask.Fill)
     document.getElementsByClassName("startModal")[0].style.display = "none"
+
+    
+    if(localStorage.getItem("logs") == null){
+        localStorage.setItem("logs", "[]")
+    }
+    currentStorage = JSON.parse(localStorage.getItem("logs")) 
 
     let fieldG = generateField(fieldSize, minesCount)
     document.body.style.overflow = "unset"
+
+    let isGameEnd = false
+    let flagsLeft = minesCount
+
+    document.getElementById("flagsLeft").textContent = flagsLeft
+
+    tick = 0
+    document.getElementById("gameResult").onclick = ()=>{
+        tick++
+        currentStorage.sort
+        if (tick ==5) {
+            for (let i = 0; i < currentStorage.length; i++) {
+                row = document.createElement("div")
+                row.innerHTML = `<div>- </div><div>⏱${currentStorage[i].score} </div> <div>💣${currentStorage[i].minesCount}</div> <div>📏${currentStorage[i].fieldSize}</div>`
+                row.classList.add("results")
+                document.getElementById("settings").appendChild(row)
+                
+            }
+        }
+    }
+
+    let scoreTimer 
+    let score = 0
+
+    scoreTimer = setInterval(() => {
+        score++
+        document.getElementById("timer").textContent = score 
+    }, 1000);
+
     function update() {
+
+        function endGame(){
+            isGameEnd = true
+            clearInterval(scoreTimer)
+            let sucessCount = 0
+            for (let j = 0; j < mask.length; j++) {
+
+                if (fieldG[j] == -1 && mask[j] == Mask.Flag) {
+                    mask[j] = Mask.sucess
+                    sucessCount++
+                }
+                if ((fieldG[j] == -1 && mask[j] != Mask.Flag && mask[j] !== Mask.sucess) || (fieldG[j] !== -1 && mask[j] == Mask.Flag)) {
+                    mask[j] = Mask.lose
+                    if (mask[j] == Mask.Flag && fieldG[j] !== -1) {
+                        fieldG[j] = Mask.Flag
+                    }
+                }
+                if (mask[j] == Mask.Fill) {
+                    mask[j] = Mask.Transparent
+                }
+            }
+
+            if(sucessCount == minesCount){
+                setTimeout(()=>{
+                    document.getElementsByClassName("startModal")[0].style.display = "flex"
+                    let gmaeResult = document.getElementById("gameResult")
+                    gmaeResult.textContent = "🚩🚩🚩"
+                    currentStorage.push({score, minesCount, fieldSize, fieldG})
+                    localStorage.setItem("logs", JSON.stringify(currentStorage))
+                },3000)
+            } 
+            if (sucessCount != minesCount) {
+                setTimeout(()=>{
+                    document.getElementsByClassName("startModal")[0].style.display = "flex"
+                    let gmaeResult = document.getElementById("gameResult")
+                    gmaeResult.textContent = "💣💣💣"
+                },3000)
+            }
+
+        }
 
         field.innerHTML = ""
         for (let i = 0; i < fieldG.length;) {
             cell = document.createElement("div")
             cell.className = "cell"
-
-
 
             cell.textContent = fieldG[i]
 
@@ -75,33 +147,71 @@ function gameStart() {
                 cell.textContent = ""
             }
 
-            if (mask[i] != Mask.Transparent) {
+            if (mask[i] != Mask.Transparent && mask[i] !== Mask.lose && mask[i] !== Mask.sucess) {
                 cell.textContent = Mask.Fill
                 cell.classList.add("fill")
+            }
+
+            if (mask[i] == Mask.Flag) {
+                cell.textContent = Mask.Flag
+            }
+            if (mask[i] == Mask.Fill) {
+                cell.textContent = Mask.Fill
+            }
+
+            if (mask[i] == Mask.sucess) {
+                if (fieldG[i] == -1) {
+                    cell.textContent = Mask.Flag
+                }
+                cell.classList.add(Mask.sucess)
+            }
+            if (mask[i] == Mask.lose) {
+
+                if (fieldG[i] != -1) {
+                    cell.textContent = Mask.Flag
+                }
+                cell.classList.add(Mask.lose)
             }
 
             cell.id = i
 
             cell.onclick = (e) => {
 
-                
+ 
 
-                if (mask[i - 1] == Mask.Transparent) {
+                const x = Math.round(i % fieldSize - 1) == -1 ? 9 : Math.round(i % fieldSize - 1)
+                const y = Math.ceil(i / fieldSize - 1)
+
+                if (mask[i - 1] == Mask.Transparent || mask[i - 1] == Mask.Flag || isGameEnd) {
                     return
                 }
-                mask[i - 1] = Mask.Transparent
-                if (fieldG[i] == -1) {
+
+                if(flagsLeft == 0 ){
+                    if(confirm("Закончить игру?")){
+                        endGame()
+                        update()
+                        return
+                    }
+                   
+                }
+               
+
+
+
+                if (fieldG[(y * fieldSize + x)] == -1) {
+
+                    endGame()
+                    update()
                     // gameOver()
                 }
 
-                const x = Math.floor(i%fieldSize)
-                const y = Math.round(i/fieldSize)+1
+
 
                 const clearing = []
 
-                function clear(x,y) {
+                function clear(x, y) {
                     if (x >= 0 && x < fieldSize && y >= 0 && y < fieldSize) {
-                        if(mask[y*fieldSize+x] == Mask.Transparent) return
+                        if (mask[y * fieldSize + x] == Mask.Transparent || mask[y * fieldSize + x] == Mask.Flag) return
                         clearing.push([x, y])
                     }
                 }
@@ -110,16 +220,74 @@ function gameStart() {
 
                 while (clearing.length) {
                     const [x, y] = clearing.pop();
-                    
-                    mask[y*fieldSize+x] = Mask.Transparent
-                    if (fieldG[(y*fieldSize+x)-1] != 0) continue
+
+                    mask[y * fieldSize + x] = Mask.Transparent
+                    if (fieldG[(y * fieldSize + x)] != 0) continue
                     clear(x + 1, y)
                     clear(x - 1, y)
                     clear(x, y + 1)
                     clear(x, y - 1)
+
                 }
 
                 update()
+            }
+
+            let timer
+
+
+            function setFlag() {
+
+               
+
+                if (mask[i - 1] == Mask.Transparent )  {
+                    return
+                }
+
+                if (mask[i - 1] == Mask.Flag) {
+                    mask[i - 1] = Mask.Fill;
+                    flagsLeft = flagsLeft + 1
+                    document.getElementById("flagsLeft").textContent = flagsLeft
+                    update()
+                    return
+                }
+                if (mask[i - 1] == Mask.Fill && flagsLeft >= 1) {
+
+                    flagsLeft = flagsLeft - 1
+                    document.getElementById("flagsLeft").textContent = flagsLeft
+                    mask[i - 1] = Mask.Flag
+
+                    update()
+                    
+                }
+
+                if(flagsLeft == 0 ){
+                    if(confirm("Закончить игру?")){
+                        endGame()
+                        update()
+                        return
+                    }
+                   
+                }
+                
+
+            }
+
+            cell.addEventListener('touchstart', () => {
+                timer = setTimeout(() => {
+                    setFlag()
+                }, 400)
+
+            })
+
+            cell.addEventListener('touchend', () => {
+                clearTimeout(timer)
+            })
+
+            cell.oncontextmenu = (e) => {
+                event.preventDefault();
+                setFlag()
+                return false
             }
 
             field.appendChild(cell)
